385ce48cbbc1d68e53af43dba18687c6fd912ad4
kwenealete <kwenait@yahoo.ca>
2024-04-17 17:00:53 +0200

    random color generator component app created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
-      <Accordian />      
+      {/* <Accordian />    */}
+
+      {/* Random Color component */}
+      <RandomColor />      
+      
     </div>
   );
 }
