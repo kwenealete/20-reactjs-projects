ea625ad68e844aeaadb7d6af2005bf8f4efb5523
kwenealete <kwenait@yahoo.ca>
2024-04-18 01:56:55 +0200

    star-rating component app to enable rating functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
@@ -10,8 +11,10 @@ function App() {
       {/* <Accordian />    */}
 
       {/* Random Color component */}
-      <RandomColor />      
-      
+      {/* <RandomColor />       */}
+
+      {/* Star Rating component */}
+      <StarRating noOfStars={10}/>
     </div>
   );
 }
