bc5bb76013f063d30bdc22ed48712d8dabe88cfd
kwenealete <kwenait@yahoo.ca>
2024-04-23 05:11:06 +0200

    image slider component app added to showcase slider functionality ability

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
@@ -14,7 +15,14 @@ function App() {
       {/* <RandomColor />       */}
 
       {/* Star Rating component */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
