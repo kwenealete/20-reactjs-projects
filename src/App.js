6e7ddea44886a20cb517dcf2e724dbe83f3abbf2
kwenealete <kwenait@yahoo.ca>
2024-05-18 11:40:41 +0200

    custom tab app component without making use of any third party library

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import menus from "./components/tree-view-menu/data"
 import QRCodeGenerator from './components/qr-code-generator';
 import LightDarkMode from './components/light-dark-mode';
 import ScrollIndicator from './components/scroll-indicator';
+import TabTest from './components/custom-tabs/tab-test';
 
 function App() {
   return (
@@ -43,7 +44,10 @@ function App() {
       {/* <LightDarkMode /> */}
 
       {/* Scroll indicator component */}
-      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"}/>
+      {/* <ScrollIndicator url={"https://dummyjson.com/products?limit=100"}/> */}
+
+      {/* Custom tabs component  */}
+      <TabTest />
     </div>
   );
 }
