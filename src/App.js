2bc14d69f821f9c167bbd46bab3df097aba756d8
kwenealete <kwenait@yahoo.ca>
2024-05-23 10:12:16 +0200

    Tic-Tac-Toe game

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import TabTest from './components/custom-tabs/tab-test';
 import ModalTest from './components/custom-modal-popup/modal-test';
 import GithubProfileFinder from './components/github-profile-finder';
 import SearchAutocomplete from './components/search-autocomplete';
+import TicTacToe from './components/tic-tac-toe';
 
 function App() {
   return (
@@ -59,7 +60,10 @@ function App() {
       {/* <GithubProfileFinder /> */}
 
       {/* Search autocomplete */}
-      <SearchAutocomplete />
+      {/* <SearchAutocomplete /> */}
+
+      {/* tic Tac Toe */}
+      <TicTacToe />
     </div>
   );
 }
